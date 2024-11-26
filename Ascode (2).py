e6371c515a56b01ff2c1dde40d22a15f3e83ec15
Assa <codermasochist@gmail.com>
2024-11-27 04:03:09 +0600

    Add files via upload

---
+++ Ascode (2).py
@@ -0,0 +1,50 @@
+from .. import loader, utils
+
+@loader.tds
+class Ascode(loader.Module):
+    """сохраняет текст в файл и читает содержимое файлов. by @codermasochist"""
+    
+    strings = {
+        "name": "Ascode",
+    }
+    
+    async def asccmd(self, message):
+        """
+        ответом на текст - сохраняет в файл.
+        """
+        args = utils.get_args_raw(message)
+        
+        reply = await message.get_reply_message()
+        if not reply:
+            return await message.reply("<emoji document_id=6327816922544997828>💀</emoji> бро, надо реплай на соо...")
+        
+        code_text = reply.message
+        
+        file_name = args if args else "code.txt"
+        
+        with open(file_name, "w") as f:
+            f.write(code_text)
+        
+        await self.client.send_file(message.peer_id, file_name)
+
+    async def readfcmd(self, message):
+        """
+        ответом на файл, достает содержимое файла.
+        """
+        reply = await message.get_reply_message()
+        if not reply or not reply.file:
+            return await message.reply("<emoji document_id=6327816922544997828>💀</emoji> бро, надо реплай на файл...")
+        
+        file = await reply.download_media()
+        
+        with open(file, "r") as f:
+            file_content = f.read()
+
+        formatted_content = file_content
+        
+        max_length = 4096
+        total_length = len(formatted_content)
+
+        for i in range(0, total_length, max_length):
+            part = formatted_content[i:i + max_length]
+            await message.reply(f"```\n{part}\n```", parse_mode="markdown")
