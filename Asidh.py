2175df0dab925ae9fbc7d8f67ca1b26291047c7d
Assa <codermasochist@gmail.com>
2024-11-27 04:02:17 +0600

    Create Asidh.py

---
+++ Asidh.py
@@ -0,0 +1,51 @@
+#meta developer: @codermasochist
+
+from telethon import events
+from .. import loader, utils
+
+@loader.tds
+class Asidh(loader.Module):
+    """Приветствие с ID нового участника. by @codermasochist"""
+
+    strings = {"name": "Asidh"}
+
+    async def client_ready(self, client, db):
+        self.client = client
+        self.db = db
+
+        @self.client.on(events.ChatAction(func=lambda e: e.user_joined or e.user_added))
+        async def handler(event):
+            user = await event.get_user()
+            chat = await event.get_chat()
+            welcome_chat_id = self.db.get(self.strings["name"], "welcome_chat_id")
+
+            if chat.id == welcome_chat_id:
+                await self.client.send_message(
+                    chat,
+                    f"<b><emoji document_id=5454390891466726015>👋</emoji> Добро пожаловать, {utils.escape_html(user.first_name)}!</b>\n"
+                    f"<b><emoji document_id=5936017305585586269>🪪</emoji> Ваш ID:</b> <code>{user.id}</code>"
+                )
+
+    @loader.command()
+    async def idh(self, message):
+        """Включает приветствие с ID."""
+        chat_id = utils.get_chat_id(message)
+        current_status = self.db.get(self.strings["name"], "welcome_chat_id", None)
+
+        if current_status == chat_id:
+            await message.respond("<b><emoji document_id=4918354603281482671>👋</emoji> Приветствие с ID включено.</b>")
+        else:
+            self.db.set(self.strings["name"], "welcome_chat_id", chat_id)
+            await message.respond("<b><emoji document_id=4918354603281482671>👋</emoji> Приветствие с ID включено.</b>")
+
+    @loader.command()
+    async def idn(self, message):
+        """Отключает приветствие с ID"""
+        chat_id = utils.get_chat_id(message)
+        current_status = self.db.get(self.strings["name"], "welcome_chat_id", None)
+
+        if current_status is None:
+            await message.respond("<b><emoji document_id=5465665476971471368>❌</emoji> Приветствие не было включено.</b>")
+        else:
+            self.db.set(self.strings["name"], "welcome_chat_id", None)
+            await message.respond("<b><emoji document_id=5877477244938489129>🚫</emoji> Приветствие с ID отключено!</b>")
