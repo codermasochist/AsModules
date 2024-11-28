f613dac389bdf2d38894ebcb754566787adf243e
Assa <codermasochist@gmail.com>
2024-11-29 03:12:06 +0600

    Update Asid.py

--- Asid.py
+++ Asid.py
@@ -1,4 +1,3 @@
-#meta developer: @codermasochist
 from .. import loader, utils
 
 @loader.tds
@@ -10,27 +9,27 @@ class AsID(loader.Module):
 
     async def идcmd(self, message):
         """
-        работает без префа, получает ид по реплаю.
+        работает без префа, просто ид
         """
         reply = await message.get_reply_message()
         args = utils.get_args(message)
 
         if not reply:
             if not args:
-                user = await self.client.get_entity(message.sender_id)
+                user = await message.client.get_entity(message.sender_id)
                 link = '<a href="t.me/{}">{}</a>'.format(user.username, user.first_name) if user.username else '<a href="tg://openmessage?user_id={}">{}</a>'.format(user.id, user.first_name)
                 return await message.reply(
                     f"<emoji document_id=5780683340810030158>✈️</emoji> {link}\n"
                     f"<emoji document_id=4918133202012340741>👤</emoji> <code>@{user.id}</code>"
                 )
-            user = await self.client.get_entity(args[0])
+            user = await message.client.get_entity(args[0])
             link = '<a href="t.me/{}">{}</a>'.format(user.username, user.first_name) if user.username else '<a href="tg://openmessage?user_id={}">{}</a>'.format(user.id, user.first_name)
             return await message.reply(
                 f"<emoji document_id=5780683340810030158>✈️</emoji> {link}\n"
                 f"<emoji document_id=4918133202012340741>👤</emoji> <code>{user.id}</code>"
             )
 
-        user = await self.client.get_entity(reply.sender_id)
+        user = await message.client.get_entity(reply.sender_id)
         link = '<a href="t.me/{}">{}</a>'.format(user.username, user.first_name) if user.username else '<a href="tg://openmessage?user_id={}">{}</a>'.format(user.id, user.first_name)
         return await message.reply(
             f"<emoji document_id=5780683340810030158>✈️</emoji> {link}\n"
@@ -39,21 +38,24 @@ class AsID(loader.Module):
 
     async def watcher(self, message):
         """
-        обрабатывает команду "ид" без префа
+        Обрабатывает команду "ид" без префикса, доступную только для владельца.
         """
+        if message.sender_id != (await message.client.get_me()).id:
+            return
+
         text = message.raw_text.strip().lower()
 
         if text == "ид":
             reply = await message.get_reply_message()
             if not reply:
-                user = await self.client.get_entity(message.sender_id)
+                user = await message.client.get_entity(message.sender_id)
                 link = '<a href="t.me/{}">{}</a>'.format(user.username, user.first_name) if user.username else '<a href="tg://openmessage?user_id={}">{}</a>'.format(user.id, user.first_name)
                 return await message.reply(
                     f"<emoji document_id=5780683340810030158>✈️</emoji> {link}\n"
                     f"<emoji document_id=4918133202012340741>👤</emoji> <code>{user.id}</code>"
                 )
 
-            user = await self.client.get_entity(reply.sender_id)
+            user = await message.client.get_entity(reply.sender_id)
             link = '<a href="t.me/{}">{}</a>'.format(user.username, user.first_name) if user.username else '<a href="tg://openmessage?user_id={}">{}</a>'.format(user.id, user.first_name)
             return await message.reply(
                 f"<emoji document_id=5780683340810030158>✈️</emoji> {link}\n"
