8e88eedd86c7dcc0d559ab75e3b566ffdb9c89f8
Assa <codermasochist@gmail.com>
2024-12-30 14:01:38 +0600

    Update As Edit`s.py

--- As Edit`s.py
+++ As Edit`s.py
@@ -12,7 +12,7 @@ class AsEditsMod(loader.Module):
     """Модуль кидает ахуенные эдиты. by @codermasochist"""
 
     strings = {
-        "name": "AsEdits",
+        "name": "As Edit`s",
         "choosi_video": "<emoji document_id=5217697679030637222>⏳</emoji> <b>подбираем эдит...</b>",
         "no_channel": "<b>no channel in config</b> <emoji document_id=5211061572306219675>🤨</emoji>",
         "no_videos_found": "<emoji document_id=5305381957524272531>❌</emoji> <b>could not find video in channel.</b>",
