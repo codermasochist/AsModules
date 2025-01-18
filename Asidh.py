d1d64b92183aa197c90cc486943a5aea6efd1319
Assa <codermasochist@gmail.com>
2025-01-18 10:47:10 +0600

    Update Asidh.py
    Signed-off-by: Assa <codermasochist@gmail.com>

--- Asidh.py
+++ Asidh.py
@@ -4,7 +4,7 @@ from telethon import events
 from .. import loader, utils
 
 @loader.tds
-class AsWelcomeId(loader.Module):
+class AsIDW(loader.Module):
     """Приветствие с ID. by @codermasochist"""
 
     strings = {"name": "As welcome ID"}
