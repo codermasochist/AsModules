f1cc4bb34067fb4b0437e93d543ea043e3a5e830
Assa <codermasochist@gmail.com>
2025-08-14 09:42:10 +0300

    Add files via upload
    Signed-off-by: Assa <codermasochist@gmail.com>

---
+++ StarsRates.py
@@ -0,0 +1,66 @@
+# meta developer: @codermasochist
+# requires: telegram-stars-rates
+
+import asyncio
+import aiohttp
+from .. import loader, utils
+from telegram_stars_rates import get_stars_rate
+
+@loader.tds
+class StarsRates(loader.Module):
+    """узнать курс тг звезд. (fragment.com)"""
+    
+    strings = {
+        "name": "Stars Rates",
+        "error": "<blockquote><emoji document_id=6050773179557745617>🫡</emoji> <b><i>ошибка получения курса... попробуйте позже.</i></b></blockquote>",
+        "result": "<blockquote><emoji document_id=5951762148886582569>⭐️</emoji> <code>{stars}</code> <b>stars</b> <emoji document_id=5897692655273383739>⭐</emoji> ≈ <code>{ton:.6f}</code> <b>ton</b>  <emoji document_id=5402104393396931859>⭐️</emoji>  <code>{usdt:.2f}</code> <b>usdt</b> <emoji document_id=5814556334829343625>🪙</emoji> <code>{rub:.2f}</code> rub<emoji document_id=5231449120635370684>💸</emoji></blockquote>",
+        "invalid": "<blockquote><emoji document_id=6037514847443227774>⭐️</emoji> <b><i>укажите правильное количество звёзд (число)</i></b></blockquote>",
+        "loading": "<blockquote><emoji document_id=6014655953457123498>💱</emoji><b> <i>получаю курс звёзд...</i></b></blockquote>"
+    }
+    
+    async def get_usdt_to_rub(self):
+        url = "https://tonapi.io/v2/rates?tokens=usdt&currencies=rub"
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    data = await response.json()
+                    return data["rates"]["USDT"]["prices"]["RUB"]
+        except:
+            return None
+
+    async def stars_to_all_currencies(self, amount):
+        usd_to_rub = await self.get_usdt_to_rub()
+        if usd_to_rub is None:
+            return None
+            
+        result = await utils.run_sync(get_stars_rate)
+        ton_per_star = result['ton_per_star']
+        usdt_per_star = result['usdt_per_star']
+
+        ton = ton_per_star * amount
+        usdt = usdt_per_star * amount
+        rub = usdt * usd_to_rub
+
+        return {
+            "stars": amount,
+            "ton": ton,
+            "usdt": usdt,
+            "rub": rub
+        }
+
+    async def srcmd(self, m):
+        """— <amount> stars. """
+        args = utils.get_args_raw(m)
+        if not args or not args.isdigit():
+            await utils.answer(m, self.strings("invalid"))
+            return
+
+        loading = await utils.answer(m, self.strings["loading"])
+        amount = int(args)
+        converted = await self.stars_to_all_currencies(amount)
+        
+        if not converted:
+            await utils.answer(m, self.strings("error"))
+            return
+
+        await utils.answer(loading, self.strings("result").format(stars=converted["stars"], ton=converted["ton"], usdt=converted["usdt"], rub=converted["rub"]))
